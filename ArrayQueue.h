cccc8be853ab7008ed275ce5ecd5e97fa2253ed4
raunaak <rklohiya1996@gmail.com>
2016-07-22 18:38:43 +0530

    Update and rename Queue.h to ArrayQueue.h

--- ArrayQueue.h
+++ ArrayQueue.h
@@ -1,14 +1,14 @@
-#ifndef QUEUE_H
-#define QUEUE_H
+#ifndef ARRAYQUEUE_H
+#define ARRAYQUEUE_H
 #include <string>
 using namespace std;
 template<class T>
-class Queue{
+class ArrayQueue{
 	T* arr; 
 	public:
 	int front; int rear; int n;
-	Queue();
-	Queue(int size);
+	ArrayQueue();
+	ArrayQueue(int size);
 	void push(const T& val);
 	T& pop();
 	T& peek();
@@ -17,40 +17,40 @@ class Queue{
 };
 
 template<class T>
-Queue<T>::Queue(){
+ArrayQueue<T>::ArrayQueue(){
 	arr = new T[10];
 	front = 0; rear = 0;
 	n = 10;
 }
 template<class T> 
-Queue<T>::Queue(int size){
+ArrayQueue<T>::ArrayQueue(int size){
 	arr = new T[size];
 	front = 0; rear = 0;
 	n = size;
 }
 template<class T>
-int Queue<T>::size(){
+int ArrayQueue<T>::size(){
 	return (n+rear-front)%n;
 }
 template<class T>
-bool Queue<T>::isEmpty(){
+bool ArrayQueue<T>::isEmpty(){
 	return size()==0;
 }
 template<class T> 
-void Queue<T>::push(const T& val){
+void ArrayQueue<T>::push(const T& val){
 	if(size()==n-1)throw string("Queue overflow");
 	arr[rear]=val;
 	rear=(rear+1)%n;
 }
 template<class T>
-T& Queue<T>::pop(){
+T& ArrayQueue<T>::pop(){
 	if(size()==0)throw string("Queue underflow");
 	T& val = arr[front];
 	front = (front+1)%n;
 	return val;
 }
 template<class T>
-T& Queue<T>::peek(){
+T& ArrayQueue<T>::peek(){
 	if(size()==0)throw string("Queue underflow");
 	return arr[front];
 }
