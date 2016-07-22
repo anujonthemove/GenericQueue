898ba44e43c9f1db5b5d71276deffb136f5b1bdc
raunaak <rklohiya1996@gmail.com>
2016-07-22 18:44:37 +0530

    Update main
    Queue class was renamed to ArrayQueue. Queue's implementation in main was thus changed to ArrayQueue.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <string>
-#include "Queue.h"
+#include "ArrayQueue.h"
 using namespace std;
 int main(){
-    Queue<int>* q = new Queue<int>();
+    /*Array based queue implementation 
+    ArrayQueue<int>* q = new ArrayQueue<int>();
     try{
     for(int i=0; i<4; i++)q->push(i);    
     for(int i=0; i<4; i++)cout<<q->pop()<<endl;
     }catch(string s){
         cout<<s<<endl;
-    }
+    }*/
     return 0;
 }
